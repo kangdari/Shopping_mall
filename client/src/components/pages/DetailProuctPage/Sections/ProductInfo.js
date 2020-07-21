0044343f80676ffef6a33f0737dee0a4dd6bf204
kangdari_ <ksh44820@naver.com>
2020-07-21 12:42:53 +0900

    add ProductInfo component DetailProdcutPage에 ProductInfo 컴포넌트 추가

--- client/src/components/pages/DetailProuctPage/Sections/ProductInfo.js
+++ client/src/components/pages/DetailProuctPage/Sections/ProductInfo.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Descriptions, Button } from 'antd';
 
 const ProductInfo = ({ product }) => {
-  return <div>ProductInfo</div>;
+  const ClickHandler = (e) => {};
+
+  return (
+    <>
+      <Descriptions title='Product Info'>
+        <Descriptions.Item label='Price'>${product.price}</Descriptions.Item>
+        <Descriptions.Item label='Sold'>{product.sold}</Descriptions.Item>
+        <Descriptions.Item label='View'>{product.views}</Descriptions.Item>
+        <Descriptions.Item label='Description'>{product.description}</Descriptions.Item>
+      </Descriptions>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button size='large' shape='round' type='danger' onClick={ClickHandler}>
+          Add to Cart
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default ProductInfo;
